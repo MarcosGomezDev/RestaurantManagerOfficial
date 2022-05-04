e38d2a9ff687bc2a659e2898fd580aecb5e272cf
Marcos Gomez <margo.med.03@gmail.com>
2022-05-04 10:46:12 +0200

    Marcos

--- RestaurantManagerOfficial/src/main/java/com/example/appbar/data/TablesData.java
+++ RestaurantManagerOfficial/src/main/java/com/example/appbar/data/TablesData.java
@@ -1,4 +1,37 @@
 package com.example.appbar.data;
 
 public class TablesData {
+
+    private int numTable;
+    private int numPersons;
+    private boolean reserved;
+
+    public TablesData(int numTable, int numPersons) {
+        this.numTable = numTable;
+        this.numPersons = numPersons;
+    }
+
+    public int getNumTable() {
+        return numTable;
+    }
+
+    public void setNumTable(int numTable) {
+        this.numTable = numTable;
+    }
+
+    public int getNumPersons() {
+        return numPersons;
+    }
+
+    public void setNumPersons(int numPersons) {
+        this.numPersons = numPersons;
+    }
+
+    public boolean isReserved() {
+        return reserved;
+    }
+
+    public void setReserved(boolean reserved) {
+        this.reserved = reserved;
+    }
 }
